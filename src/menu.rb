e6ac9a10a3cf3a7234c8d066dd2ffffddc704aca
gomaxxy <gomaxxy@gmail.com>
2013-05-03 05:12:14 +0000

    Menu cleaned up a bit. Rules category vacated for incoming rules.

--- src/menu.rb
+++ src/menu.rb
@@ -5,9 +5,11 @@
 #   activates contained plugins.
 # 
 # Created 4/5/2013
-# Last updated 4/29/2013
+# Last updated 5/2/2013
 
 require 'sketchup.rb'
+require 'rules.rb'
+
 
 
 # Create new menu item under "Plugins"
@@ -16,43 +18,30 @@ rb_submenu = rb_menu.add_submenu("RuleBear")
 
 # MAIN MENUS
 
-# Test Menu
-test_menu = rb_menu.add_submenu("Test")
+  # Test Menu
+  test_menu = rb_menu.add_submenu("Test")
 
+  # Lego menu
+  lego_menu = rb_submenu.add_submenu("Select a Lego")
 
-# Rules submenu
-rule_menu = rb_submenu.add_submenu("Rules")
+  # Rules submenu
+  rule_menu = rb_submenu.add_submenu("Select a Rule")
   
-
-# Rule Maintenance submenu
-rule_maint_menu = rb_submenu.add_submenu("Rule Maintenance")
-
-
-
-# RULES MAINTENANCE SUBMENU OPTIONS
-
-  # Rules > Create New Rule
-  rule_create = rule_maint_menu.add_item("Create a Rule") {
-  create_input  # method draws input box
-  }
-
-  # Rules > Edit Rule 
-  # Rule Creation box with fields populated appears.  
-  rule_edit = rule_maint_menu.add_item("Edit a Rule") {
-    UI.messagebox "Rule editing nonfunctional at this time."
+  # Create New Rule
+  rule_create = rb_submenu.add_item("Create a Rule") {
+    create_input  # method draws input box
   }
 
   # Rules > Delete Rule
-  rule_delete = rule_maint_menu.add_item("Delete a Rule") {
+  rule_delete = rb_submenu.add_item("Delete a Rule") {
     UI.messagebox "Rule deletion here"    
   }
 
 
+
 # LEGO RULES MENU 
 
-  # LEGO MENU
 
-  lego_menu = rule_menu.add_submenu("Rule 0: Select Lego")
 
 
   #LEGO SUBMENU
@@ -76,43 +65,24 @@ rule_maint_menu = rb_submenu.add_submenu("Rule Maintenance")
   }
 =end
 
-  # PLACEMENT RULES MENU
-
-  # Menu: Rule 1
-  rule1 = rule_menu.add_item("Rule 1: Place base Lego") {
-    rule_1   
-  }
-
-  # Menu: Rule 2
-  rule2 = rule_menu.add_item("Rule 2: Place 2x2 on 2x2") {
-    rule_2   
-  }
-    
-  # Menu: Rule 3
-  rule3 = rule_menu.add_item("Rule 3: Place 2x2 on 2x4") {
-    rule_3    
-  }
- 
-  # Menu: Rule 4
-  rule4 = rule_menu.add_item("Rule 4: Place 2x4 on 2x2") {
-    rule_4   
-  }
-
-  # Menu: Rule 5
-  rule5 = rule_menu.add_item("Rule 5: Place 2x4 on 2x4") {
-    rule_5   
-  }
 
  
 # TEST MENU ITEMS
 
   test_instance = test_menu.add_item("Test Instance"){
-  test_add_instance
+    test_add_instance
   }
 
   print_stuff = test_menu.add_item("Print Selected Object IDs"){
-  printstuff
+    printstuff
   }
 
+  #TEST RULE. DELETE WHEN RULES ARE ADDED TO MENU
 
+  # Menu: Rule 1
+  rule1 = rule_menu.add_item("Place Lego at side") {
+    rule_1   
+  }
+
+  
 
