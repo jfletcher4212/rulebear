b8128fa1476d9cd99d84919736f6f05a6496f7c9
gomaxxy <gomaxxy@gmail.com>
2013-05-04 21:31:56 +0000

    requires file contains all 'require' statements for new user-created rules. This file should be placed inside RuleBear folder on desktop

---
+++ src/rb_requires.rb
@@ -0,0 +1,18 @@
+# rulebear.rb
+# Primary author: Maxine Major
+# Created: 5/4/2013
+# Last Modified: 5/4/2013
+# This file contains all the requires for the RuleBear SketchUp plugin.
+# All these requires point to a folder "RuleBear" on the user's desktop.
+# Due to SketchUp's limited permissions, unable to write to Plugins folder.
+# RuleBear needs: 
+#   This file in the Plugins folder.
+#   All other RuleBear files are in this folder
+#   New RuleBear rules are in the folder on the desktop.
+
+
+require 'sketchup.rb'
+
+#additional require statements appended below
+
+
