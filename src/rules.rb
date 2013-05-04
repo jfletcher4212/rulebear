5f13e6f7ce0ddc168d0e31fef4d0b8c17516ece6
gomaxxy <gomaxxy@gmail.com>
2013-05-04 21:31:08 +0000

    Updated rules file. Functionality includes rule creation to RuleBear file on desktop. file path needs to be adjusted manually for user. rules created here are functional as an individual entity. Do not affect lego placement at this time. Deletion of a rule is functional. Need to create verification prior to delete.

--- src/rules.rb
+++ src/rules.rb
@@ -5,15 +5,19 @@
 #   input boxes for rule creation
 # 
 # Created 4/16/2013
-# Last updated 5/2/2013
+# Last updated 5/4/2013
 
 require 'sketchup.rb'
 
-# Sketchup.send_action "showRubyPanel"
+# global path variables
+$pc_plugins_dir = "C:\Program Files (x86)\Google\Google SketchUp 8\Plugins"
+$mac_plugins_dir = "/Library/Application Support/Google SketchUp 8/SketchUp/plugins"
+$rb_filepath = "C:\\Users\\Maxine\\Desktop\\RuleBear"
+
 
 #Notes:
 # $selectedpiece is current piece
-# Redefined variables for rule-making
+# Redefined variables for rule-making:
   $lego2x4 = $piece1    # $piece2 is 2x2 
   $lego2x2 = $piece2    # $piece1 is 2x4 
 # $targetpiece = ??
@@ -24,10 +28,10 @@ $rule_objs = Array.[]( "2x2Lego ", "2x4Lego " ) # object identifiers
 
 
 
-# ----- USER INPUT BOX -----
 
-# CREATE INPUT BOX
+# CREATE INPUT BOX -------------------------------------------------- 
 def create_input
+
 
   # DEFINE INPUT BOX
   # Note: drop down box for prompts that have pipe-delimited lists of options.
@@ -64,70 +68,106 @@ def create_input
                              attributes_list, 
                              "Create a Rule"   # title of input box
 
-  # Initialize variables with user input values
+  #test input box creation
+  if (ruleinputbox)                     # If box creation is successful
+    initialize_boxvars( ruleinputbox )  # turn user inputs into variables
+
+  else
+    UI.messagebox "Error: Create a rule could not be launched."
+   
+  end
+
+end
+
+
+# Initialize variables with user input values from input box
+def initialize_boxvars( ruleinputbox )
+
     $rulename    = ruleinputbox[0]
     $ruledesc    = ruleinputbox[1]
     $ruleobj     = ruleinputbox[2]
     $rulebaseobj = ruleinputbox[3]
     $ruleloc     = ruleinputbox[4]
+    $fname       = "#{$rulename}.rb" # rule file will be rulename.rb 
 
   test_inputs
 
 end
 
 
-# TEST INPUTS
+# TEST INPUTS: check for valid rule inputs.
 def test_inputs
+
+   # TEST RULE NAME
    
   if( $rulename == "" )
     UI.messagebox "Rule Name is invalid.\nCould not create rule."
-  else
-    # print input values for testing purposes only
-    # this section will get deleted in final code submission
-    UI.messagebox "Rule name: #{$rulename}\n
-                   Description: #{$ruledesc}\n
-                   Object: #{$ruleobj}\n
-                   Target Obj: #{},
-                   Location: #{$ruleloc}"
 
+
+  elsif ( File.exist?(File.join($rb_filepath, "#{$fname}")) )     
+    UI.messagebox "ERROR: Rule name \"#{$rulename}\" already exists. 
+                     Cannot create this rule."
+    
+=begin
+  else  # print input values for testing purposes
+    UI.messagebox "Create rule: #{$rulename}\nDescription: #{$ruledesc}\nObject: #{$ruleobj}\nTarget Obj: #{},Location: #{$ruleloc}"
+=end
+
+  end
+
+  # TEST RULE INPUTS: Ensure that at either object, target obj, or location 
+    # has been selected. If none of these are selected, 
+    # then we have no criteria for establishing a rule.
+
+  if( $ruleobj == "" and $rulebaseobj == "" and $ruleloc =="" )
+    UI.messagebox "ERROR: no criteria selected with which to create a rule"
+
+  else
+    #UI.messagebox "We have enough criteria to make a rule"
+    
     create_rule_file #send rule to get either created or discarded
 
   end
 
+
+
 end
 
 
-
 # ----- WRITE RULE TO FILE -----
-# this code adds all variables created by input box to end of rules file
+# this code creates a new rule based on user input rulename
+# and uses all input variables created by input box to create a rule
 
 def create_rule_file
 
-  # the user's filename input will become a filename.rb file 
-  # if a rule exists by that same name, we do not create it.
-  fname = "#{$rulename}.rb" # user input rulename will be rulename.rb
-  oktocreate = 0            # create rule? 0 = no / 1 = yes
+      
+      # File.open("#{fname}", 'w+')  # Create file for writing-to
+      File.open(File.join($rb_filepath, "#{$fname}"), 'w+')
 
- 
-    if File.exist?(fname) # if a rule by the same name exists
-      oktocreate = 0      # we do not make a new file
-      UI.messagebox "ERROR: Rule name \"#{$rulename}\" already exists. Cannot create this rule."
-    else                  # rule does not already exist
-      oktocreate = 1      # Ok to create rule file
-      File.open("#{fname}", 'w+')  # Create file for writing-to
-      write_rule (fname)  # write the rule to file
-    end
+      # verify location of this file
+      filepath = File.expand_path(File.join($rb_filepath, "#{$fname}")) 
+      
+      write_rule  # write the rule to file
 
 end
 
 
 # WRITE RULE TO FILE with user input rule criteria
-def write_rule( fname )
+def write_rule
 
+  # rule variables
   time = Time.new # use this to create timestamp for rule file
-  
+  menuname =  "#{$rulename}"+"_menu"
+  menudesc = "#{$rulename}".capitalize
+  defname  = "#{$rulename}"+"_def"
+
   # open file with append - best to append rule as we parse input box
-  accessfile = File.open("#{fname}", 'a')  
+  unless ( File.exist?(File.join($rb_filepath, "#{$fname}")) ) 
+    UI.messagebox "Apparently the file #{$fname} doesn't exist."
+
+  else # write to file
+
+  accessfile = File.open(File.join($rb_filepath, "#{$fname}"), 'a')
 
   # header descriptions
   accessfile.puts("\# Rule created in RuleBear.")
@@ -135,20 +175,54 @@ def write_rule( fname )
   accessfile.puts("\# #{$ruledesc}")  
   accessfile.puts("")
   
+  #require
+  accessfile.puts("require \'sketchup.rb\'")
+  accessfile.puts("require \'C\:\\Program Files \(x86\)\\Google\\Google SketchUp 8\\Plugins\\menu.rb\'") #use pc_plugins_dir
+  accessfile.puts("")  
+
+  # add rule to menu
+  accessfile.puts("\# add #{$rulename} to menu") 
+  accessfile.puts("UI.menu\(\"PlugIns\"\).add_item\(\"#{menudesc}\"\)\{") 
+  accessfile.puts("  #{defname}")
+  accessfile.puts("\}")  
+  accessfile.puts("")  
+
+
   # create the new rule
-  accessfile.puts("def #{$rulename}")
-  accessfile.puts("  UI.messagebox \"yay! my rule works!\" \# comment")
+  accessfile.puts("def #{defname}")
+  accessfile.puts("  UI.messagebox \"yay! #{$rulename} works!\" \# comment")
   accessfile.puts("end")
   accessfile.puts("")
 
-  # add rule to menu
-  
+  # finished writing to our rules file
+  # close the file 
+  accessfile.close
+
+
+  end
+
+  # add to requires menu
+
+  unless ( File.exist?( File.join( $rb_filepath, "rb_requires.rb" ) ) ) 
+    UI.messagebox "can\'t find the requires file"
+
+  else # write to file
+    accessrequires = File.open(File.join($rb_filepath, "rb_requires.rb"), 'a')
+    accessrequires.puts("require \'#{$rb_filepath}\\#{$fname}\'")
+
+
+
+  end
+
+
+
 
 
   # close the file 
-  accessfile.close
-end
+  accessrequires.close
 
+  
+end
 
 
   
@@ -158,6 +232,34 @@ end
 
 
 # DELETE A RULE
-  # be conservative with this one
+  #currently does not delete rule from requires file
+
+def delete_rule
+
+  #input box for rule to delete
+  
+  prompts = ["Name of Rule to Delete: "]
+  defaults = ["rulename"] 
+  attributes_list = [""]
+
+  deletebox = UI.inputbox prompts, 
+                          defaults, 
+                          attributes_list, 
+                          "Delete a Rule"   # title of input box
+
+  #test input box creation
+  if (deletebox)                     # If box creation is successful
+    deletefile = "#{deletebox[0]}"+".rb"
+    UI.messagebox "will be deleting #{deletefile}"
+  else
+    UI.messagebox "Error: Create a rule could not be launched."
+   
+  end
+
+  #verify if rule deletion is necessary
+  
+  # delete file
+  File.delete( File.join( $rb_filepath, "#{deletefile}" ) ) # yes, it's that easy
 
 
+end
